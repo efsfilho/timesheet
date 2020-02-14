092a47e1e2dbfacfff68a4e7c99e279a244fb720
Eduardo Filho <efdsfilho@gmail.com>
2020-02-13 22:00:24 -0300

    update

--- config/index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  logDir: './log/', // local dos logs
-  exportDir: './exports/', // local dos exports do xlsx
-  exportModelFileName: './src/file.xlsx', // arquivo excel modelo para exportacao
-  AWS_REGION: 'us-east-1'
-};
